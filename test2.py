865bd360b636bfe8eac9ede7208d0ff6bd9ceb1a
ray <rui.song90@gmail.com>
2023-10-25 22:23:30 +0100

    first commit

--- test2.py
+++ test2.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @Filename:    test.py.py
+# @Filename:    test2.py.py
 # @Author:      Dr. Rui Song
 # @Email:       rui.song@physics.ox.ac.uk
-# @Time:        25/10/2023 22:10
-
-
-import os
-import
-
+# @Time:        25/10/2023 22:23
